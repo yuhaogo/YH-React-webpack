0e7c8534f5e1c773d139acc096ba9c49f5a92c5b
yuhaogo <yuhaogo@163.com>
2017-08-15 11:36:21 +0800

    logger

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
+// var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs=require('ejs');
@@ -14,7 +14,7 @@ app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
